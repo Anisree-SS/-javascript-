f6717ff130f669b2419ea25ecd6fd4b69496a02d
Anisree S S <anisree.ss@techversantinfotech.com>
2024-01-14 22:20:29 +0530

    committing

--- Q43/script/script.js
+++ Q43/script/script.js
@@ -1,15 +1,26 @@
 function find()
 {
 	var area=0;
-	var h=parseInt(document.getElementById("num1").value);
-	var l=parseInt(document.getElementById("num2").value);
-	var r=parseInt(document.getElementById("num3").value);
-	area=(3.14*r*l)+(3.14*(r*r));
-	v=(1/3)*3.14*(r*r)*h;
-	ls=3.14*r*l;
-	document.getElementById("demo").innerHTML = "Total Surface Area of a Cube: "+area;
-	document.getElementById("demo1").innerHTML = "Volume of a Cube: "+v;
-	document.getElementById("demo2").innerHTML = "Lateral Surface Area of a Cube : "+ls;	
+	var num1=document.getElementById("num1").value;
+	var num2=document.getElementById("num2").value;
+	var num3=document.getElementById("num3").value;
+	var no=/^[A-z@~`!@#$%^&*()_=+\\\\';:\"\\/?>.<,-]*$/i;
+	cal=1;
+	cal=cal+num1.match(no);
+	if((num1.charAt(0)==" ")||(cal!="1")||(num1==""))
+		alert("invalid input!!");
+	else
+	{	
+		h=parseInt(num1);
+		l=parseInt(num2);
+		r=parseInt(num3);
+		area=(3.14*r*l)+(3.14*(r*r));
+		v=(1/3)*3.14*(r*r)*h;
+		ls=3.14*r*l;
+		document.getElementById("demo").innerHTML = "Total Surface Area of a Cube: "+area;
+		document.getElementById("demo1").innerHTML = "Volume of a Cube: "+v;
+		document.getElementById("demo2").innerHTML = "Lateral Surface Area of a Cube : "+ls;	
+	}
 }
 	
 
--- Q45/script/script.js
+++ Q45/script/script.js
@@ -1,15 +1,26 @@
 function find()
 {
 	var area=0;
-	var h=parseInt(document.getElementById("num1").value);
-	var l=parseInt(document.getElementById("num2").value);
-	var w=parseInt(document.getElementById("num3").value);
-	area=(2*l*w)+(2*l*h)+(2*w*h);
-	v=l*w*h;
-	ls=(2*h)*(l+w);
-	document.getElementById("demo").innerHTML = "Total Surface Area of a Cube: "+area;
-	document.getElementById("demo1").innerHTML = "Volume of a Cube: "+v;
-	document.getElementById("demo2").innerHTML = "Lateral Surface Area of a Cube : "+ls;	
+	var num1=document.getElementById("num1").value;
+	var num2=document.getElementById("num2").value;
+	var num3=document.getElementById("num3").value;
+	var no=/^[A-z@~`!@#$%^&*()_=+\\\\';:\"\\/?>.<,-]*$/i;
+	cal=1;
+	cal=cal+num1.match(no);
+	if((num1.charAt(0)==" ")||(cal!="1")||(num1==""))
+		alert("invalid input!!");
+	else
+	{	
+		h=parseInt(num1);
+		l=parseInt(num2);
+		w=parseInt(num3);
+		area=(2*l*w)+(2*l*h)+(2*w*h);
+		v=l*w*h;
+		ls=(2*h)*(l+w);
+		document.getElementById("demo").innerHTML = "Total Surface Area of a Cube: "+area;
+		document.getElementById("demo1").innerHTML = "Volume of a Cube: "+v;
+		document.getElementById("demo2").innerHTML = "Lateral Surface Area of a Cube : "+ls;	
+	}
 }
 	
 
