c05ccd833402226ba14ef2a691d1123a8ce38184
Anisree S S <anisree.ss@techversantinfotech.com>
2024-01-14 23:45:53 +0530

    committing

--- Q45/script/script.js
+++ Q45/script/script.js
@@ -6,8 +6,8 @@ function find()
 	var num3=document.getElementById("num3").value;
 	var no=/^[A-z@~`!@#$%^&*()_=+\\\\';:\"\\/?>.<,-]*$/i;
 	cal=1;
-	cal=cal+num1.match(no);
-	if((num1.charAt(0)==" ")||(cal!="1")||(num1==""))
+	cal=cal+num1.match(no)+num2.match(no)+num3.match(no);
+	if((num1.charAt(0)==" ")||(cal!="1")||(num1=="")||(num2.charAt(0)==" ")||(num2=="")||(num3.charAt(0)==" ")||(num3==""))
 		alert("invalid input!!");
 	else
 	{	
