c6b4f99cc5badd077466471087e23b46fc77e64a
Anisree-SS <anisree.ss@techversantinfotech.com>
2024-01-17 15:21:05 +0530

    committing of 17/01/2024

--- Q20/script/script.js
+++ Q20/script/script.js
@@ -1,10 +1,20 @@
-function display(val){
-	document.getElementById("number").value += val;
+function display(val) {
+   document.getElementById("number").value += val;
 }
 
-function reset(){
-	document.getElementById("number").value="";
+function reset() {
+   document.getElementById("number").value = "";
 }
-	
 
-		
+function calculate() {
+   var x = document.getElementById('number').value;
+   let y = eval(x);
+   document.getElementById('number').value = y;
+}
+
+
+function deleteData() {
+   data = document.getElementById("number").value;
+   data = data.substring(0, data.length - 1);
+   document.getElementById("number").value = data;
+}
